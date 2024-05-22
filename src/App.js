12c6bef5d15c694a09ff2d92deb7ecc00f31fe02
brunomangueira <151276504+brunomangueira@users.noreply.github.com>
2024-05-22 01:29:05 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState }  from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './componentes/Home';
+import Login from './componentes/Login';
+import Signup from './componentes/Signup';
+import './componentes/style/style.css'; // Importação do arquivo de estilos CSS
+import PasswordResetRequest from './Request-Password-Reset';
+import PasswordReset from './Password-Reset';
+import NavbarLogado from './componentes/Home-Logado';
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/request-password-reset" element={<PasswordResetRequest />} />
+        <Route path="/reset-password" element={<PasswordReset />} />
+        <Route path="/homeLogado" element={<NavbarLogado />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ src/componentes/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from './Paginas/Pictures/Pura aqcua ABA.png'; // Certifique-se de que o caminho está correto
+import './style/Navbar.css'; // Importar o arquivo CSS
+
+function Navbar({ isAuthenticated }) {
+  const handleLogout = () => {
+    // Lógica para fazer logout
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light custom-navbar">
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          <img src={logo} alt="Logo" className="navbar-logo" />
+          Pura Acqua Brasil
+        </NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/">Home</NavLink>
+            </li>
+            {isAuthenticated ? (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link" activeClassName="active" to="/profile">Perfil</NavLink>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link" onClick={handleLogout}>Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" activeClassName="active" to="/signup">Cadastre-se</NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
